0d3826c0869e4d186bdb35a04ff6384881871331
rodrigorgrb <rodrigorgrb@gmail.com>
2019-05-17 23:56:37 -0300

    Caminhando para fazer 1030

---
+++ ucoder/1030.rb
@@ -0,0 +1,24 @@
+entrada = gets.split
+qfita = entrada[0].to_i
+qtd = entrada[1].to_i
+fita = []
+dias = []
+
+for t in (0...qfita)
+  fita << 0
+end
+
+##inicial
+is = gets.split
+is = is.map{|i| i.to_i}
+
+##dias
+for x in (1..qtd)
+  puts x
+end
+
+=begin
+13 3
+2 6 13
+=end
+
