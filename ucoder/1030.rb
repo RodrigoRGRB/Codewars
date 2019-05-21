6c96e0b31b90c3b815a2d35511bd4a82757642eb
rodrigorgrb <rodrigorgrb@gmail.com>
2019-05-20 23:35:37 -0300

    Quase la

--- ucoder/1030.rb
+++ ucoder/1030.rb
@@ -8,15 +8,77 @@ for t in (0...qfita)
   fita << 0
 end
 
-##inicial
-is = gets.split
-is = is.map{|i| i.to_i}
+def teste(posicao, tamanho, fita)
+  anterior = posicao - 1
+  atual = posicao
+  proxima = posicao + 1
+  puts "\n\n\n\n"
+  if anterior < 0
+    fita.insert((proxima - 1), 1)
+    fita.delete_at(proxima)
+    fita.insert((atual - 1), 1)
+    fita.delete_at(atual)
 
-##dias
-for x in (1..qtd)
-  puts x
+  elsif proxima > tamanho
+    fita.insert((atual - 1), 1)
+    fita.delete_at(atual)
+
+    fita.insert((anterior - 1), 1)
+    fita.delete_at(anterior)
+  else
+    fita.insert((atual - 1), 1)
+    fita.delete_at(atual)
+
+    fita.insert((anterior - 1), 1)
+    fita.delete_at(anterior)
+
+    fita.insert((proxima - 1), 1)
+    fita.delete_at(proxima)
+  end
+    fita
 end
 
+#inicial
+is = gets.split
+is.map do |i| 
+  i.to_i
+  fita.insert((i.to_i - 1), 1)
+  fita.delete_at(i.to_i)
+end
+
+is = is.map do |i| 
+  i.to_i
+end
+
+teste = 1
+def atualiza sujo, antigo
+  antigo.each do |i|
+    anterior = i - 1
+    proximo = i + 1
+    sujo << anterior
+    sujo << proximo
+  end
+  print sujo
+  sujo
+end
+##dias
+while fita.include?0
+  is = atualiza is, is
+
+  is.each do |i|
+    fita = teste(i.to_i, fita.length, fita)
+  end
+
+  print "\n #{is}"
+  print "\nfita no dia #{teste} fita = #{fita}"
+
+  teste+=1
+  break
+end
+
+print fita
+print fita.length
+print "\n dias #{teste}"
 =begin
 13 3
 2 6 13
